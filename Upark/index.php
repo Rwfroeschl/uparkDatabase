2a301e35bfac1789c53c98f3990824daeac2f8ad
Robert Froeschl <rwfroeschl@gmail.com>
2024-04-29 14:22:09 -0500

    update to database project

--- Upark/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config.php';  // Include your connection script
-
-// Attempt a query execution to further confirm the connection
-$query = $pdo->query("SELECT * FROM Users");  // Replace 'some_table' with a real table name
-while ($row = $query->fetch()) {
-    echo $row['Name'];  // Replace 'column_name' with a real column name
-}
-?>
